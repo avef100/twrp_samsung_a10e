428311b0cbee2591484d2bbcd9107843c7073cd2
avef100 <159497493+avef100@users.noreply.github.com>
2024-05-22 11:26:14 -0400

    Create A10e.mk

---
+++ A10e.mk
@@ -0,0 +1,6 @@
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base.mk)
+PRODUCT_NAME := a10e
+PRODUCT_DEVICE := a10e
+PRODUCT_BRAND := samsung
+PRODUCT_MODEL := A102U1
+PRODUCT_MANUFACTURER := samsung
