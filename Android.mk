1271ae9f22dced51266cd564fba2ec76c637f126
avef100 <159497493+avef100@users.noreply.github.com>
2024-05-22 11:31:57 -0400

    Create Android.mk

---
+++ Android.mk
@@ -0,0 +1,9 @@
+LOCAL_PATH := $(call my-dir)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE := a10e
+LOCAL_SRC_FILES := proprietary/vendor.img
+LOCAL_MODULE_CLASS := ETC
+LOCAL_MODULE_PATH := $(TARGET_OUT_VENDOR)
+LOCAL_MODULE_TAGS := optional
+include $(BUILD_PREBUILT)
