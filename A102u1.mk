6882c80502f9415bf1a95d33ab97cbc35f8618e5
avef100 <159497493+avef100@users.noreply.github.com>
2024-05-22 12:26:36 -0400

    Update and rename A10e.mk to A102u1.mk

--- A102u1.mk
+++ A102u1.mk
@@ -1,6 +1,7 @@
+# a102u1.mk
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base.mk)
-PRODUCT_NAME := a10e
-PRODUCT_DEVICE := a10e
+PRODUCT_NAME := a102u1
+PRODUCT_DEVICE := a102u1
 PRODUCT_BRAND := samsung
-PRODUCT_MODEL := A102U1
+PRODUCT_MODEL := SM-A102U1
 PRODUCT_MANUFACTURER := samsung
